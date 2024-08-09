6b62641035bb98937e8b9e9c017b2564ea44f758
loaisaber07 <loai.sabir@outlook.com>
2024-08-09 16:28:54 +0300

    Ahmed Kaml CoueseDetails view & CourseController& CourseViewModel

---
+++ UdemyMVC/Controllers/CourseController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using UdemyMVC.Models;
+using UdemyMVC.Repositories;
+using UdemyMVC.ViewModels;
+
+namespace UdemyMVC.Controllers
+{
+    public class CourseController : Controller
+    {
+        ICourseRepository courseRepository;
+        public CourseController(ICourseRepository courseRepository)
+        {
+            this.courseRepository = courseRepository;
+        }
+        //async methoud should be awaited =>  use async Task 
+
+        public async Task<IActionResult> CourseDetailes(int id)
+        {
+            Course course = await courseRepository.GetCoursesByCourseIdAsync(id);
+
+            if (course == null)
+            {
+                return NotFound();
+            }
+            decimal sumRate = course.CourseRates.Select(c => c.Rate).Sum();
+            int countRate = course.CourseRates.Select(c => c.Rate).Count();
+            ViewBag.title = "Test Test Test";
+            CourseDetailsVM courseVM = new CourseDetailsVM();
+            courseVM.Id = course.ID;
+            courseVM.CourseName = course.CourseName;
+            courseVM.Title = course.Title ?? ViewBag.title;
+            courseVM.Description = course.Description;
+            courseVM.CourseImg = course.CourseImage;
+            courseVM.Price = course.Price;
+            courseVM.chapters = course.Chapters.Where(c => c.CourseId == id).ToList();
+            courseVM.Rating = (sumRate / countRate).ToString() ?? "1";
+            courseVM.NumberOfStudent = course.Enrollment.Count();
+            courseVM.Instructor = course.Instructor.User.FullName;
+            courseVM.Category = course.CategoryCourses.Select(c => c.Category.CategoryName).ToList();
+
+            return View("CourseDetailes", courseVM);
+        }
+    }
+}
---
+++ UdemyMVC/ViewModels/CourseDetailsVM.cs
@@ -0,0 +1,20 @@
+﻿using UdemyMVC.Models;
+
+namespace UdemyMVC.ViewModels
+{
+    public class CourseDetailsVM
+    {
+        public int Id { get; set; }
+        public string CourseName { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public int Price { get; set; }
+        public string CourseImg { get; set; }
+        public string Instructor { get; set; }
+        public List<string> Category { get; set; }
+        public int NumberOfStudent { get; set; }
+        public string Rating { get; set; }
+        public List<Chapter> chapters { get; set; }
+
+    }
+}
