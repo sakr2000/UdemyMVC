fbe22bd29efa9fb51fee0b811721e6e5667b8056
sakr2000 <sakr83117@gmail.com>
2024-08-11 02:43:32 +0300

    Finished landingpage sliders

---
+++ UdemyMVC/wwwroot/js/home.js
@@ -0,0 +1,39 @@
+﻿$('.owl-carousel.logos').owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: false,
+    dots: false,
+    autoplay: true,
+    responsive: {
+        0: {
+            items: 1
+        },
+        600: {
+            items: 3
+        },
+        1000: {
+            items: 6
+        }
+    }
+})
+
+
+$(".owl-carousel.courses").owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: false,
+    dots: false,
+    autoplay: true,
+    responsiveClass: true,
+    responsive: {
+        0: {
+            items: 1
+        },
+        600: {
+            items: 3
+        },
+        1000: {
+            items: 4,
+        }
+    }
+})
